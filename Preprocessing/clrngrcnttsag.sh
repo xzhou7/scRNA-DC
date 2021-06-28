3da7d9304a97bbb5c399360afbb0c54d987f03d5
Xin Zhou <34285739+xzhou7@users.noreply.github.com>
2021-06-28 11:57:33 -0700

    Add files via upload

---
+++ Preprocessing/clrngrcnttsag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --mem=50G
+#SBATCH -t 7-00:00:00
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user rvnair@stanford.edu
+#SBATCH -A baas_lab_mpsnyder
+#SBATCH -o %x.o%j
+date
+hostname
+module load cellranger/2.2.0
+cellranger count --id=ST20 --transcriptome=$HG19 --fastqs=/BaaS/labs/mpsnyder/Rabinovitch/Toshie/cellranger_mkfastq/HW373BBXX/outs/fastq_path --sample=ST20 --expect-cells=2000
+echo 'Done'
